159a3189ae7891a8e8ff3edf6524ecd3e6186be0
Niklas Hegnelt <wispster@gmail.com>
2020-05-11 21:19:32 +0200

    add writePnL Zorro script

---
+++ zorro-script/writePnL.c
@@ -0,0 +1,49 @@
+/* Small snippet of code to write open trades to csv file
+
+1. Place this file in your Zorro include folder and
+
+2. Place the following in the top of your trade script
+#define LOG_PNL "C:\\inetpub\\wwwroot\\logs\\my-pnl.csv"
+#include <writePnL.c>
+
+
+*/
+#include <stdio.h>
+#include <default.c>
+
+void writePnL(string fileName, int minHour)
+{
+    static var oldPnL = 0;
+    static bool written = true;
+    static int lastDay = -1;
+	string tradeDirection;
+    // only if file does not exist
+    vars ft;
+    if ((ft = fopen(fileName, "r"))) {
+        fclose(ft);
+    } else {
+        vars fp = fopen(fileName, "w");
+        fprintf(fp, "%s", "Date,PnL\n");
+        fclose(fp);
+    }
+
+	if((ldow() <= 5) and (lhour() >= minHour) and (day() != lastDay)) {
+        var PnL = (Equity - oldPnL);        
+        vars fp = fopen(fileName, "a");
+        fprintf(fp, "%04i-%02i-%02i,%.2f,%.2f\n", 
+            year(), 
+            month(), 
+            day(),
+            Equity - oldPnL,
+            oldPnL);
+        fclose(fp);
+        oldPnL = Equity;
+        lastDay = day();
+    }
+}
+
+function tock() {
+	if (!is(LOOKBACK)) {
+		writePnL(LOG_PNL, 15);
+	}
+}
